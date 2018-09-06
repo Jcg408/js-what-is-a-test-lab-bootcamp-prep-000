ecb8d837f6fefa801c14a0ba78a699dff489b624
Jessica Gladstone <jcg321@comcast.net>
2018-09-06 18:19:37 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -14,12 +14,11 @@ describe('what-is-a-test', () => {
     })
   })
 
- /* describe('Message', () => {
+  describe('Message', () => {
     it('gives the name and height', () => {
       expect(index.message).toInclude(index.name)
       expect(index.message).toInclude(index.height)
     })
-    */
   })
 
 })
